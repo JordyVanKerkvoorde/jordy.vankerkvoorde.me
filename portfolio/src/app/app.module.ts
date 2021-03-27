c532e91528cabf696f1209af418ef4eb06d8f7d4
Jordy <jordy.vankerkvoorde@student.hogent.be>
2021-03-27 19:18:24 +0100

    Add components and file structure

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ProjectComponent } from './components/projects-page/project/project.component';
+import { PackageComponent } from './components/projects-page/package/package.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomePageComponent,
+    ProjectsPageComponent,
+    NavigationComponent,
+    ProjectComponent,
+    PackageComponent,
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
